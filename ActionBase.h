e817cf67617cefce222e6aeb17fe671bb5858ba4
Sven Bursch-Osewold <sb_git@bursch.com>
2016-12-14 15:51:12 +0100

    Reformat

--- ActionBase.h
+++ ActionBase.h
@@ -16,14 +16,20 @@ public:
 	virtual ~ActionBase();
 	//virtual int loop();
 	virtual void TurnoutCmd(int id, int direction, int source);
+
 	// Reports the Loco Speed and Direction
 	virtual void DCCSpeed(int id, int speed, int direction, int SpeedSteps, int source);
+
 	// Reports just the changed Bit
 	virtual void DCCFunc(int id, int bit, int newvalue, int source);
+
 	// Reports the Status of the all functions (bit 0 => F0, bit 1 => F1, ...)
 	virtual void DCCFunc(int id, unsigned long int newvalue, int source);
+
 	virtual String getHTMLCfg(String urlprefix);
 	virtual String getHTMLController(String urlprefix);
+
+	// Calls from the Webserver for post and get requests
 	virtual void setSettings(String key, String value);
 
 };
