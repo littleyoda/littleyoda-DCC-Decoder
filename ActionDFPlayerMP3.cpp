e3fe4d5ab0675ba6dcc2c0dc8ec4d0142c30093c
Sven Bursch-Osewold <sb_git@bursch.com>
2017-05-31 21:47:15 +0200

    Mp3: Clean up

--- ActionDFPlayerMP3.cpp
+++ ActionDFPlayerMP3.cpp
@@ -9,53 +9,45 @@
 #include <SoftwareSerial.h>
 #include "Arduino.h"
 #include <ESP8266WiFi.h>
+#include "Consts.h"
 
 
-#include "DFPlayer_Mini_Mp3.h"
-
-ActionDFPlayerMP3::ActionDFPlayerMP3(int s1, int s2) {
-	Serial.print("Pins RX: " + String(s1) + "/ TX:" + String(s2));
+ActionDFPlayerMP3::ActionDFPlayerMP3(int addr, int rx, int tx) {
+	Serial.print("Pins RX: " + String(rx) + "/ TX:" + String(tx));
+	locoId = addr;
 	delay(2000);
-	mp3Serial = new SoftwareSerial(s1, s2);
+	mp3Serial = new SoftwareSerial(tx, rx);
 	mp3Serial->begin(9600);
-	mp3_set_serialZeiger(mp3Serial);
-	mp3_set_debug_serial(Serial);
-	mp3_set_volume(13);
-
-//	 mp3_set_device(2);
-//	 mp3_get_tf_sum ();
-
-//	  for (int i=0; i < 2; i++) {
-//		  Serial.println("Seinding");
-//	    // Query the total number of microSD card files
-//	    mp3_get_folder_sum(i + 1);
-//	    int count  = mp3_wait_folder_sum();
-//
-//	    Serial.print("Find ");
-//	    Serial.print(count);
-//	    Serial.print(" tracks in folder 0");
-//	    Serial.print(i);
-//	    Serial.println(".");
-//	}
+	initTime = millis() + 2000;
 }
+
 
 ActionDFPlayerMP3::~ActionDFPlayerMP3() {
 }
 
 int ActionDFPlayerMP3::loop() {
-	int count = 0;
-	while (true) {
-		int b = mp3Serial->read();
-		if (b == -1) {
-			if (count > 0) {
-				Serial.println();
-			}
-			return 0;
+	if (initTime > 0 && millis() > initTime) {
+		initTime = 0;
+		cmdSelectSDCard();
+	}
+	if (mp3Serial->available() < 1) {
+		return 50;
+	}
+	while (mp3Serial->available()> 0) {
+		if (millis() - lastReceived > 200) {
+			bufferPos = 0; // verwerfe alten Buffer
 		}
+		int b = mp3Serial->read();
 		Serial.print(b, HEX);
 		Serial.print(" ");
+		ansbuf[bufferPos++] = (uint8_t) b;
+		if (bufferPos == 10) {
+			checkReceiveBuffer();
+			bufferPos = 0;
+		}
+		lastReceived = millis();
 	}
-	return 0;
+	return 50;
 }
 
 String ActionDFPlayerMP3::getHTMLCfg(String urlprefix) {
@@ -67,7 +59,7 @@ String ActionDFPlayerMP3::getHTMLController(String urlprefix) {
 	message += "<div class=\"column column-10\">Songs</div>";
 	message += "<div class=\"column column-90\">";
 
-	for (int i = 0; i <= 10; i++) {
+	for (int i = 1; i <= 10; i++) {
 		message += " <a class=\"button\" href=\"";
 		message += urlprefix;
 		message += "key=play&value=";
@@ -98,17 +90,89 @@ String ActionDFPlayerMP3::getHTMLController(String urlprefix) {
 
 }
 
-void ActionDFPlayerMP3::setSettings(String key, String value) {
-	if (key.equals("play")) {
-		Serial.println("Playing " + value);
-		mp3_play(value.toInt());
-	} else 	if (key.equals("volume")) {
-		Serial.println("Volume " + value);
-		mp3_set_volume(value.toInt());
-	}
-//	for (int i = 0; i < 10; i++) {
-//		mp3Serial -> write(play1[i]);
-//	}
-//	mp3Serial -> write(play1);
+void ActionDFPlayerMP3::cmdSelectSDCard() {
+	sendCommand(0x09, 0x02);
 }
 
+void ActionDFPlayerMP3::cmdPlay(int number) {
+	sendCommand(0x03, number);
+}
+
+void ActionDFPlayerMP3::cmdVolume(int volume) {
+	if (volume > 30) {
+		volume = 30;
+	}
+	if (volume < 0) {
+		volume = 0;
+	}
+	sendCommand(0x06, volume);
+}
+
+void ActionDFPlayerMP3::sendCommand(uint8_t cmd, uint16_t data) {
+  sendBuffer[0] = 0x7e;   // Start Byte
+  sendBuffer[1] = 0xff;   // Version information
+  sendBuffer[2] = 0x06;   // Length -2 (without Start and End-Byte
+  sendBuffer[3] = cmd;//
+  sendBuffer[4] = 0x01;   // Feedback
+  sendBuffer[5] = (uint8_t)(data >> 8);  // first Databyte
+  sendBuffer[6] = (uint8_t)(data);       // second Databyte
+  sendBuffer[7] = 0xef;   // End-Byte
+  mp3Serial->write(sendBuffer, 8);
+}
+
+
+void ActionDFPlayerMP3::DCCSpeed(int id, int speed, int direction, int SpeedSteps, int source) {
+	if (id == locoId || id == Consts::LOCID_ALL) {
+		if (speed == Consts::SPEED_EMERGENCY || speed == Consts::SPEED_STOP) {
+			cmdStop();
+			return;
+		}
+		int v = 30 * speed / SpeedSteps;
+		cmdVolume(v);
+	}
+}
+
+void ActionDFPlayerMP3::DCCFunc(int id, int bit, int newvalue, int source) {
+	Serial.println("Bit: " + String(bit) + " Newvalue: " + String(newvalue));
+	if (bit == 0) {
+		cmdStop();
+		return;
+	}
+	if (id == locoId && newvalue > 0) {
+		cmdPlay(bit);
+	}
+}
+
+
+void ActionDFPlayerMP3::setSettings(String key, String value) {
+	if (key.equals("play")) {
+		Serial.println("Playing " + String(value));
+		cmdPlay(value.toInt());
+	} else 	if (key.equals("volume")) {
+		Serial.println("Volume " + value);
+		cmdVolume(value.toInt());
+	}
+}
+
+void ActionDFPlayerMP3::cmdStop() {
+	sendCommand(0x16, 0x00);
+}
+
+void ActionDFPlayerMP3::checkReceiveBuffer() {
+		bool valid =  ansbuf[0] == 0x7E
+						&& ansbuf[1] == 0xFF
+						&& ansbuf[2] == 0x06
+						&& ansbuf[9] == 0xEF;
+	  if (!valid) {
+		  return;
+	  }
+	  if (ansbuf[3] == 0x3A && ansbuf[6] == 0x02) {
+		  Serial.println("SD card plugged in");
+	  } else if (ansbuf[3] == 0x3B && ansbuf[6 == 0x02]) {
+		  Serial.println("SD card plugged out");
+	  } else if (ansbuf[3] == 0x41) {
+		  Serial.println("Command received");
+	  } else if (ansbuf[3] == 0x3D) {
+		  Serial.println("Finish playing! " + String(ansbuf[4] << 16 | ansbuf[5] << 8 | ansbuf[6]));
+	  }
+}
--- ActionDFPlayerMP3.h
+++ ActionDFPlayerMP3.h
@@ -13,17 +13,29 @@
 
 class ActionDFPlayerMP3: public ActionBase {
 public:
-	ActionDFPlayerMP3(int s1, int s2);
+	ActionDFPlayerMP3(int addr, int rx, int tx);
 	virtual int loop();
 	virtual ~ActionDFPlayerMP3();
 	virtual String getHTMLCfg(String urlprefix);
 	virtual String getHTMLController(String urlprefix);
 	virtual void setSettings(String key, String value);
+	virtual void DCCSpeed(int id, int speed, int direction, int SpeedSteps, int source);
+	virtual void DCCFunc(int id, int bit, int newvalue, int source);
 
 private:
+	virtual void cmdSelectSDCard();
+	virtual void cmdStop();
+	virtual void cmdPlay(int number);
+	virtual void cmdVolume(int volume);
+	virtual void sendCommand(uint8_t command, uint16_t dat);
+	virtual void checkReceiveBuffer();
 	SoftwareSerial* mp3Serial;
-	uint8_t play1[10] =
-			{ 0X7E, 0xFF, 0x06, 0X03, 00, 00, 0x01, 0xFE, 0xF7, 0XEF };
+	uint8_t sendBuffer[8];
+	uint8_t ansbuf[10];
+	int bufferPos = 0;
+	int locoId;
+	unsigned long lastReceived = 0;
+	unsigned long initTime = 0;;
 };
 ;
 
