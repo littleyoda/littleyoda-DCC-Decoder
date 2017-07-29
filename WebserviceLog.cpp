e66f3e02dd147e6078e9917d43174b83ba97b8fa
Sven Bursch-Osewold <sb_git@bursch.com>
2017-07-29 16:29:55 +0200

    [Imp] Speicherverbrauch gesenkt

--- WebserviceLog.cpp
+++ WebserviceLog.cpp
@@ -23,28 +23,33 @@ char const* WebserviceLog::getUri() {
 }
 
 void WebserviceLog::run() {
+	String end = F("</td></tr>\n");
+	String start = F("<tr><td>");
 	sendBasicHeader();
 	send(Utils::getHTMLHeader());
 	send("<table border=\"1\"><tr><th>Attribut</th><th>Status</th></tr>\n");
-	send("<tr><td>Memory (free/free at startup)</td><td>" + String(ESP.getFreeHeap()) + "/"
-			+ String(Logger::getInstance()->startmemory) + " Bytes </td></tr>\n");
-	send("<tr><td>Uptime</td><td>" + String(millis() / 1000)
-			+ " sek</td></tr>\n");
-	send("<tr><td>Wifi RSSI</td><td>" + String(WiFi.RSSI())
-			+ "</td></tr>\n");
-	send("<tr><td>Wifi Status</td><td>"
-			+ Utils::wifi2String(WiFi.status()) + "</td></tr>\n");
-	send("<tr><td>Wifi SSID</td><td>" + WiFi.SSID() + "</td></tr>");
-	send("<tr><td>IP (Access Point)</td><td>" + WiFi.softAPIP().toString() + "</td></tr>");
-	send("<tr><td>IP (WLAN Client)</td><td>" + WiFi.localIP().toString() + "</td></tr>");
+	send(start + "Memory (free/free at startup)</td><td>" + String(ESP.getFreeHeap()) + "/"
+			+ String(Logger::getInstance()->startmemory) + " Bytes" + end);
+	send(start + "Uptime</td><td>" + String(millis() / 1000)
+			+ " sek" + end);
+	send(start + "Version</td><td>" + String(compile_date)
+			+ end);
+	send(start + "Wifi RSSI</td><td>" + String(WiFi.RSSI())
+			+ end);
+	send(start + "Wifi Status</td><td>"
+			+ Utils::wifi2String(WiFi.status()) + end);
+	send(start + "Wifi SSID</td><td>" + WiFi.SSID() + end);
+	send(start + "IP (Access Point)</td><td>" + WiFi.softAPIP().toString() + end);
+	send(start + "IP (WLAN Client)</td><td>" + WiFi.localIP().toString() + end);
 
 	  LinkedList<Logger::logdata*>* list = Logger::getInstance()->getLogs();
 	for (int i = 0; i < list->size(); i++) {
-		send("<tr><td>Log ");
+		send(start);
+		send(F("Log "));
 		send(String(i));
 		send("</td><td>");
 		send(list->get(i)->msg);
-		send("</td></tr>\n");
+		send(end);
 	}
 	send("</table>");
 	send(Utils::getHTMLFooter());
