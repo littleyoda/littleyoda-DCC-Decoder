adf842ba447654b8394a7da8193e8849fcfb0b32
Sven Bursch-Osewold <sb_git@bursch.com>
2017-08-06 19:57:29 +0200

    [NEW] Non-Blocking WIFI Scan hinzugefügt

--- littleyoda-DCC-Decoder.ino
+++ littleyoda-DCC-Decoder.ino
@@ -55,6 +55,7 @@ void loadCFG(Webserver* web) {
 }
 
 
+bool scanRunning = false;
 void handleSerial() {
 	if (Serial.available() > 0) {
 		int chr = Serial.read();
@@ -63,7 +64,7 @@ void handleSerial() {
 			Serial.println("==================");
 			Serial.println("Free start memory: " + String(Logger::getInstance()->startmemory));
 			Serial.println("Free memory: " + String(ESP.getFreeHeap()));
-			Serial.println("\nLogger:");
+			Serial.println("\r\nLogger:");
 			Serial.println("==================");
 			Serial.println("Logger: " + String(Logger::getInstance()->getMemUsage()));
 			if (controller->dccSniffer != NULL) {
@@ -72,13 +73,15 @@ void handleSerial() {
 			if (controller->cmdlogger != NULL) {
 				Serial.println("Commandlogger: " + String(controller->cmdlogger->getMemUsage()));
 			}
-			Serial.println("\nWifi:");
+			Serial.println("\r\nWifi:");
 			Serial.println("==================");
 			Serial.println("IP: " + Utils::wifi2String(WiFi.status()) + "  / " + WiFi.localIP().toString());
 			WiFi.printDiag(Serial);
 			Serial.println(WiFi.softAPIP().toString());
+			Serial.println("Qualität (RSSI): " + String(WiFi.RSSI()));
+			Serial.printf("Verbunden mit: %s\r\n", WiFi.BSSIDstr().c_str());
 
-			Serial.println("\nFlash:");
+			Serial.println("\r\nFlash:");
 			Serial.println("==================");
 
 			// Taken from https://github.com/esp8266/Arduino/blob/master/libraries/esp8266/examples/CheckFlashConfig/CheckFlashConfig.ino
@@ -93,15 +96,72 @@ void handleSerial() {
 			FlashMode_t ideMode = ESP.getFlashChipMode();
 			Serial.printf("Speed/Mode: %u %s", ESP.getFlashChipSpeed(), (ideMode == FM_QIO ? "QIO" : ideMode == FM_QOUT ? "QOUT" : ideMode == FM_DIO ? "DIO" : ideMode == FM_DOUT ? "DOUT" : "UNKNOWN"));
 			Serial.println();
-		} else if (chr == 'r') {
+		} else if (chr == 'R') {
 			ESP.restart();
 		} else if (chr == 'D') {
 			Serial.println("Configfile deleted");
 			SPIFFS.remove("/config.json");
 		} else if (chr == 'a') {
 			controller->enableAPModus();
+		} else if (chr == 'r') {
+			WiFi.reconnect();
+		} else if (chr == 'w') {
+			Serial.println("Scan Started");
+			WiFi.scanNetworks(true, true);
+			scanRunning = true;
 		} else {
 			Serial.println("Key: " + String(chr));
+		}
+	}
+	if (scanRunning) {
+		if (WiFi.scanComplete() == WIFI_SCAN_FAILED) {
+			Serial.println("Scan Failed!");
+			scanRunning = false;
+		}
+		if (WiFi.scanComplete() >= 0) {
+			int n = WiFi.scanComplete();
+			for (int i = 0; i < n; ++i) {
+				String message = "";
+				String ssid_scan;
+				int32_t rssi_scan;
+				uint8_t sec_scan;
+				uint8_t* BSSID_scan;
+				int32_t chan_scan;
+				bool hidden_scan;
+
+				WiFi.getNetworkInfo(i, ssid_scan, sec_scan, rssi_scan, BSSID_scan, chan_scan, hidden_scan);
+				message +="[";
+				for (int i = 0; i < 6; i++) {
+					if (BSSID_scan[i] < 16) {
+						message += "0";
+					}
+					message += String(BSSID_scan[i], HEX);
+					if (i < 5) {
+						message +=":";
+					}
+
+				}
+				message +="] CH:";
+				if (chan_scan < 10) {
+					message += "0";
+				}
+				message += String(chan_scan);
+				message += " ";
+				message += (WiFi.encryptionType(i) == ENC_TYPE_NONE) ? "OPEN" : "ENC ";
+				message += "  Q:";
+				message += String(WiFi.RSSI(i));
+				message += "  --- ";
+				message += ssid_scan;
+				message += "  ";
+				if (hidden_scan) {
+					message += " (HIDDEN)";
+				}
+				message += "\r\n";
+				Serial.print(message);
+			}
+			Serial.printf("BSSID: %s\r\n", WiFi.BSSIDstr().c_str());
+			WiFi.scanDelete();
+			scanRunning = false;
 		}
 	}
 }
