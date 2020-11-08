1c03d25f5b7ed5269c59a4f1db5ec514ae8f00b9
Sven Bursch-Osewold <sb_git@bursch.com>
2020-11-08 14:15:37 +0100

    [etc] i2c Scanner hinzugefügt

--- decoder/decoder.cpp
+++ decoder/decoder.cpp
@@ -212,39 +212,38 @@ void handleSerial() {
 				Serial.println(l->msg);
 			}
 		} else if (chr == '2') {
-//  	int nDevices = 0;
-// 	 uint8_t address,error; 
-// 	 Wire.begin(D4, D2);
-//   for(address = 1; address < 127; address++ ) 
-//   {
-//     // The i2c_scanner uses the return value of
-//     // the Write.endTransmisstion to see if
-//     // a device did acknowledge to the address.
-//     Wire.beginTransmission(address);
-//     error = Wire.endTransmission();
+			int nDevices = 0;
+			uint8_t address,error; 
+			Wire.begin();
+			for(address = 1; address < 127; address++ ) {
+				// The i2c_scanner uses the return value of
+				// the Write.endTransmisstion to see if
+				// a device did acknowledge to the address.
+				Wire.beginTransmission(address);
+				error = Wire.endTransmission();
 
-//     if (error == 0)
-//     {
-//       Serial.print("I2C device found at address 0x");
-//       if (address<16) 
-//         Serial.print("0");
-//       Serial.print(address,HEX);
-//       Serial.println("  !");
+				if (error == 0)
+				{
+				Serial.print("I2C device found at address 0x");
+				if (address<16) 
+					Serial.print("0");
+				Serial.print(address,HEX);
+				Serial.println("  !");
 
-//       nDevices++;
-//     }
-//     else if (error==4) 
-//     {
-//       Serial.print("Unknow error at address 0x");
-//       if (address<16) 
-//         Serial.print("0");
-//       Serial.println(address,HEX);
-//     }    
-//   }
-//   if (nDevices == 0)
-//     Serial.println("No I2C devices found\n");
-//   else
-// Serial.println("done\n");
+				nDevices++;
+				}
+				else if (error==4) 
+				{
+				Serial.print("Unknow error at address 0x");
+				if (address<16) 
+					Serial.print("0");
+				Serial.println(address,HEX);
+				}    
+			}
+			if (nDevices == 0)
+				Serial.println("No I2C devices found\n");
+			else
+			Serial.println("done\n");
 		} else if (chr == 'm') {
 			Serial.printf("Pointer %d\r\n", sizeof(String*));
 			Serial.printf("String (without Buffer) %d\r\n", sizeof(String));
